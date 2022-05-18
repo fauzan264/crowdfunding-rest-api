e9f38500008876e9287832e6c726c026a87b11ea
ahmad fauzan <fauzan.c4@gmail.com>
2022-05-19 00:44:32 +0700

    get data in database

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import(
+	"crowdfunding-rest-api/user"
 	"fmt"
 	"log"
 
@@ -10,11 +11,24 @@ import(
 
 func main() {
 	dsn := "root:@tcp(127.0.0.1:3306)/db_crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Connection to database success")
+
+	// create variable for struct
+	var users []user.User
+
+	// find db
+	db.Find(&users)
+
+	for _, user := range users {
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+		fmt.Println("==================")
+	}	
+
 }
---
+++ user/entity.go
@@ -0,0 +1,18 @@
+package user
+
+import "time"
+
+type User struct {
+	ID string
+	Name string
+	Email string
+	Password string
+	Occupation string
+	Image string
+	Role string
+	Token string
+	CreatedBy string
+	UpdatedBy string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
