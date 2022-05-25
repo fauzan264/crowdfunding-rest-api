31c19942141f89b0081bb0e6ebe5c9f1e3050ea0
ahmad fauzan <fauzan.c4@gmail.com>
2022-05-26 03:11:24 +0700

    main.go rapih-rapih

--- main.go
+++ main.go
@@ -12,9 +12,6 @@ import (
 )
 
 func main() {
-	// router := gin.Default()
-	// router.GET("/user", handlerUser)
-	// router.Run()
 	
 	dsn := "root:@tcp(127.0.0.1:3306)/db_crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -35,21 +32,3 @@ func main() {
 
 	router.Run()
 }
-
-// func handlerUser(c *gin.Context) {
-// 	dsn := "root:@tcp(127.0.0.1:3306)/db_crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
-// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-// 	if err != nil {
-// 		log.Fatal(err.Error())
-// 	}
-
-// 	// create variable for struct
-// 	var users []user.User
-
-// 	// find db
-// 	db.Find(&users)
-
-// 	// convert to JSON
-// 	c.JSON(http.StatusOK, users)
-// }
