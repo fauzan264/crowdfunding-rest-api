74b6e27da9943f273c7273e13ed3bb66d7535a90
ahmad fauzan <fauzan.c4@gmail.com>
2022-08-24 02:06:01 +0700

    add entity campaing

--- campaign/entity.go
+++ campaign/entity.go
@@ -1,1 +1,30 @@
 package campaign
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Campaign struct {
+	Id               uuid.UUID
+	UserId           uuid.UUID
+	ShortDescription string
+	Description      string
+	Perks            string
+	BackerCount      int
+	GoalAmount       int
+	CurrentAmount    int
+	Slug             string
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+}
+
+type CampaignImage struct {
+	Id         uuid.UUID
+	CampaignId uuid.UUID
+	FileName   string
+	IsPrimary  int
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
